62c6224ed210040e363d6ae99a03a70433957793
logan03109 <wonglogan69@gmail.com>
2026-06-09 19:50:50 +0100

    controllers

--- scripts/main.gd
+++ scripts/main.gd
@@ -35,6 +35,7 @@ var first_safe_chunk := true
 var current_chunk: Node2D = null
 
 func _ready():
+	print(Input.get_connected_joypads())
 	print("test with logan for git")
 	add_to_group("main")
 	camera.zoom = Vector2(3.0, 3.0)
@@ -91,7 +92,6 @@ func _process(delta):
 	decay_line.global_position.x = decay_wall_x - 1100
 	
 	if in_safe_chunk:
-		print("in safe chunk")
 		safe_timer -= delta
 
 		timer_label.text = "Decay resumes in: %.1f" % safe_timer
--- scripts/player_1.gd
+++ scripts/player_1.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @export var jump_action := "p1_jump"
 @export var down_action := "p1_down"
 @export var dash_action := "p1_dash"
+@export var device_id := 0
+
 @onready var anim = $AnimatedSprite2D
 
 const SPEED = 200.0
@@ -35,58 +37,121 @@ func _die():
 	get_tree().call_group("main", "_on_player_died", self)
 
 func _physics_process(delta):
+
 	if global_position.y > DEATH_Y:
 		_die()
 		return
+
 	if dash_timer > 0.0:
 		dash_timer -= delta
 	if dash_cooldown_timer > 0.0:
 		dash_cooldown_timer -= delta
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	if is_dashing:
-		velocity.x = dash_direction * DASH_SPEED
-		velocity.y = 0.0
-		if dash_timer <= 0.0:
-			is_dashing = false
-		move_and_slide()
-		return
+
 	if is_on_floor():
 		jump_count = 2
-	
-	if Input.is_action_just_pressed(jump_action) and jump_count > 0:
-		jump_count -= 1 
-		velocity.y = JUMP_VELOCITY	
-	
-	if Input.is_action_just_pressed(down_action) and not is_on_floor():
-		velocity.y = velocity.y - DOWN_VELOCITY
-	
-	if Input.is_action_just_pressed(dash_action) and dash_cooldown_timer <= 0.0:
+
+	# -------------------------
+	# KEYBOARD INPUT (existing)
+	# -------------------------
+	var kb_left := Input.is_action_pressed(left_action)
+	var kb_right := Input.is_action_pressed(right_action)
+	var kb_jump := Input.is_action_just_pressed(jump_action)
+	var kb_down := Input.is_action_just_pressed(down_action)
+	var kb_dash := Input.is_action_just_pressed(dash_action)
+
+	# -------------------------
+	# CONTROLLER INPUT
+	# -------------------------
+	var axis_x := Input.get_joy_axis(device_id, JOY_AXIS_LEFT_X)
+
+	var pad_left := axis_x < -0.2
+	var pad_right := axis_x > 0.2
+
+	var pad_jump := Input.is_joy_button_pressed(device_id, JOY_BUTTON_A) # X
+	var pad_dash := Input.is_joy_button_pressed(device_id, JOY_BUTTON_RIGHT_SHOULDER) # R1
+	var pad_down := Input.is_joy_button_pressed(device_id, JOY_BUTTON_LEFT_SHOULDER) # L1
+
+	# -------------------------
+	# MERGED INPUTS
+	# -------------------------
+	var left := kb_left or pad_left
+	var right := kb_right or pad_right
+	var jump := kb_jump or pad_jump
+	var down := kb_down or pad_down
+	var dash := kb_dash or pad_dash
+
+	# -------------------------
+	# DASH
+	# -------------------------
+	if dash and dash_cooldown_timer <= 0.0:
 		is_dashing = true
 		dash_timer = DASH_DURATION
 		dash_cooldown_timer = DASH_COOLDOWN
-		var dir := Input.get_axis(left_action, right_action)
-		dash_direction = dir if dir != 0.0 else dash_direction
 
-	var direction := Input.get_axis(left_action, right_action)
+		var dir := 0
+		if left: dir -= 1
+		if right: dir += 1
 
-	if direction and not is_on_floor():
-		velocity.x = move_toward(velocity.x, direction * SPEED, INIT_ACCEL * delta)
-		anim.play("run")
+		if axis_x != 0:
+			dir = sign(axis_x)
+
+		dash_direction = dir if dir != 0 else dash_direction
+
+	if is_dashing:
+		velocity.x = dash_direction * DASH_SPEED
+		velocity.y = 0.0
+
+		if dash_timer <= 0.0:
+			is_dashing = false
+
+		move_and_slide()
+		return
+
+	# -------------------------
+	# JUMP
+	# -------------------------
+	if jump and jump_count > 0:
+		jump_count -= 1
+		velocity.y = JUMP_VELOCITY
+
+	# -------------------------
+	# DOWN
+	# -------------------------
+	if down and not is_on_floor():
+		velocity.y -= DOWN_VELOCITY
+
+	# -------------------------
+	# MOVEMENT
+	# -------------------------
+	var direction := 0
+
+	if left:
+		direction -= 1
+	if right:
+		direction += 1
+
+	if abs(axis_x) > 0.2:
+		direction = sign(axis_x)
+
+	if direction != 0:
+		if not is_on_floor():
+			velocity.x = move_toward(velocity.x, direction * SPEED, INIT_ACCEL * delta)
+			anim.play("jump")
+		else:
+			velocity.x = move_toward(velocity.x, direction * SPEED, INIT_ACCEL * delta)
+			anim.play("run")
+
 		anim.flip_h = direction < 0
-		anim.play("jump")
 
-	elif direction:
-		velocity.x += 50
-		velocity.x = move_toward(velocity.x, direction * SPEED, INIT_ACCEL * delta)
-		anim.play("run")
-		anim.flip_h = direction < 0
-	elif not is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, AIR_RESISTANCE * delta)
-		anim.play("jump")
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
-		anim.play("idle")
+		if not is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, AIR_RESISTANCE * delta)
+			anim.play("jump")
+		else:
+			velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+			anim.play("idle")
 
 	move_and_slide()
